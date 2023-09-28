e30238258ba3928dd5be0b6ccb5ae8e62ce44f07
Jan Obernberger <j4n.obernberger@gmail.com>
2023-09-28 20:41:58 +0200

    init

---
+++ lua/custom/init.lua
@@ -0,0 +1,4 @@
+vim.cmd("colorscheme catppuccin-macchiato")
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+require("lspconfig").clangd.setup({ capabilities = capabilities })
